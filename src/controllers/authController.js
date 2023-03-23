d66614d6354cf594011a619ef2d81e398eda1338
NguyenDangMinhNhat <nnht133@gmail.com>
2023-03-23 13:01:05 +0700

    Create register view

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,8 +4,13 @@ module.exports = {
     login: (req, res, next) => {
         res.render('auth/login', { title: 'Login' });
     },
+
     authenticate: passport.authenticate('local', {
         successRedirect: '/',
         failureRedirect: '/login'
-    })
+    }),
+
+    register: (req, res, next) => {
+        res.render('auth/register', { title: 'Register' });
+    },
 }
